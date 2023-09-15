31a35cf981d60a2a242993f0b7a81e322272ab52
mendoncaDS <donca.p.r@gmail.com>
2023-09-15 14:27:34 -0300

    unite mobile and desktop versions into one

--- database_dashboard.py
+++ database_dashboard.py
@@ -13,15 +13,15 @@ from sqlalchemy import create_engine, text
 
 
 load_dotenv()
-database_password = os.environ.get('database_password')
-server_address = os.environ.get('server_address')
+database_password = os.environ.get('password')
+server_address = os.environ.get('serverip')
 
 connection_string = f"postgresql://postgres:{database_password}@{server_address}:5432/postgres"
 engine = create_engine(connection_string,echo=False)
 
 
 
-def plot_in_placeholder(series, placeholder, symbol=None):
+def plot_in_placeholder(series, placeholder):
 
     if not isinstance(series, pd.Series):
         st.error("The input is not a Pandas Series.")
@@ -36,10 +36,10 @@ def plot_in_placeholder(series, placeholder, symbol=None):
     df.columns = ["Date", "Value"]
 
     # Create interactive plot using plotly.express
-    fig = px.line(df, x='Date', y='Value', title=symbol)
+    fig = px.line(df, x='Date', y='Value')
 
     # Update the layout to adjust the height and width
-    fig.update_layout(autosize=True,height=750)  # You can adjust the width and height values as needed
+    fig.update_layout(autosize=True,height=650)  # You can adjust the width and height values as needed
 
     placeholder.plotly_chart(fig,use_container_width=True,theme="streamlit")
 
@@ -124,16 +124,127 @@ def unique_symbol_freqs():
 
 
 def main():
-    
-    # Setting the layout to wide
-    st.set_page_config(layout="wide")
-    
+    st.set_page_config(
+        page_title="Binance Dashboard",
+        page_icon="graph-emoji.png",
+        layout="wide",
+    )
+
+    # Initialize to 'Select version' by default
+    if 'version' not in st.session_state:
+        st.session_state.version = ''
+
+    # Only show 'Choose a version' prompt on main page if version hasn't been selected or is 'Select version'
+    if st.session_state.version == '':
+        st.write("Are you on Desktop or Mobile?")
+        version_options = ['', 'Desktop', 'Mobile']
+        version = st.selectbox('Choose a version:', version_options, index=0)
+        
+        # If user selects a valid version, update the session state
+        if version in ['Desktop', 'Mobile']:
+            st.session_state.version = version
+            st.experimental_rerun()  # Rerun the script to load the new content
+        else:
+            st.stop()
+
     if 'selected_symbol' not in st.session_state:
         st.session_state.selected_symbol = None
     if 'selected_freq' not in st.session_state:
         st.session_state.selected_freq = None
 
-    st.title("Binance Prices")
+    # Rest of your code
+    if st.session_state.version == 'Desktop':
+        pages = {
+            "📈 Market Prices": prices_page_desktop,
+            "🤖 Bots": bots_page,
+        }
+
+        st.sidebar.title("Navigation")
+        page = st.sidebar.radio("Go to", list(pages.keys()))
+        st.sidebar.markdown("<hr/>", unsafe_allow_html=True)
+        pages[page]()
+    elif st.session_state.version == 'Mobile':
+        pages = {
+            "📈 Market Prices": prices_page_mobile,
+            "🤖 Bots": bots_page,
+        }
+
+        st.sidebar.title("Navigation")
+        page = st.sidebar.radio("Go to", list(pages.keys()))
+        st.sidebar.markdown("<hr/>", unsafe_allow_html=True)
+        pages[page]()
+        
+
+
+    
+def prices_page_desktop():
+    st.title("📈 Binance Market Prices")
+
+
+    if st.session_state.selected_symbol is not None:
+        graph_name_placeholder = st.empty()
+        graph_name_placeholder.write(f"💲 {st.session_state.selected_symbol}")
+    else:
+        graph_name_placeholder = st.empty()
+        graph_name_placeholder.write("💲")
+
+    # Placeholder for the graph
+    graph_placeholder = st.empty()
+
+    # If there's old data in session state, plot it before querying new data
+    if hasattr(st.session_state, "graph_data") and st.session_state.graph_data is not None:
+        plot_in_placeholder(st.session_state.graph_data["close"], graph_placeholder)
+
+    st.sidebar.title("Data to fetch:")
+    # Move form to sidebar
+    with st.sidebar.form(key='my_form'):
+        # Symbol and Frequency selectors are inside the form
+        symbol_freq_selector()
+
+        selected_datetime = datetime_selector()
+
+        # The submit button for the form
+        submit_button = st.form_submit_button(label='Load Data')
+
+    # Check if the form was submitted
+    if submit_button:
+
+        most_recent_dt = get_last_timestamp(st.session_state.selected_symbol, st.session_state.selected_freq)
+        
+        if not is_combination_available(st.session_state.selected_symbol, st.session_state.selected_freq):
+            st.warning(f"The combination {st.session_state.selected_symbol} and {st.session_state.selected_freq} is not available.")
+        elif selected_datetime and selected_datetime > most_recent_dt:
+            st.warning("Please select a date that's before the most recent data available.")
+        else:
+            try:
+                data = load_symbol_freq_data(
+                    st.session_state.selected_symbol,
+                    st.session_state.selected_freq,
+                    selected_datetime
+                )
+                if len(data) > 0:
+                    graph_name_placeholder.write(f"💲 {st.session_state.selected_symbol}")
+                    st.session_state.graph_data = data  # Update the data in the session state
+                    plot_in_placeholder(st.session_state.graph_data["close"], graph_placeholder)  # Re-plot with new data
+                else:
+                    st.warning("Please select an older start date.")
+            except Exception as e:
+                st.warning("Something went wrong.")
+                st.error(f"PYTHON ERROR: {e}")
+
+    elif hasattr(st.session_state, "graph_data") and st.session_state.graph_data is not None:
+        # If the form hasn't been submitted in this rerun but there's stored data, plot the stored data
+        plot_in_placeholder(st.session_state.graph_data["close"], graph_placeholder)
+
+def prices_page_mobile():
+    st.title("📈 Binance Market Prices")
+
+    if st.session_state.selected_symbol is not None:
+        graph_name_placeholder = st.empty()
+        graph_name_placeholder.write(f"💲 {st.session_state.selected_symbol}")
+    else:
+        graph_name_placeholder = st.empty()
+        graph_name_placeholder.write("💲")
 
     # Placeholder for the graph
     graph_placeholder = st.empty()
@@ -142,9 +253,9 @@ def main():
     if hasattr(st.session_state, "graph_data") and st.session_state.graph_data is not None:
         plot_in_placeholder(st.session_state.graph_data["close"], graph_placeholder, st.session_state.selected_symbol)
 
-    st.sidebar.title("Data to fetch:")
     # Move form to sidebar
-    with st.sidebar.form(key='my_form'):
+    with st.form(key='my_form'):
+        st.title("Data to fetch:")
         # Symbol and Frequency selectors are inside the form
         symbol_freq_selector()
 
@@ -181,6 +292,10 @@ def main():
         # If the form hasn't been submitted in this rerun but there's stored data, plot the stored data
         plot_in_placeholder(st.session_state.graph_data["close"], graph_placeholder, st.session_state.selected_symbol)
 
+
+def bots_page():
+    st.title("🤖 Bots")
+    st.subheader("Coming soon!")
 
 if __name__ == "__main__":
     main()
