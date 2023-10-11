9c8d81bb77a78f0468de0b490dd333661a063c4d
mendoncaDS <donca.p.r@gmail.com>
2023-10-11 07:29:15 -0300

    added indicators page

--- auxiliary.py
+++ auxiliary.py
@@ -4,6 +4,7 @@ import pytz
 
 import time as t
 import pandas as pd
+import pandas_ta as ta
 import streamlit as st
 import plotly.express as px
 
@@ -78,6 +79,43 @@ def unique_symbol_freqs(engine):
     return unique_combinations
 
 # ---------------------- Data Processing Functions ----------------------
+
+def process_indicators(graph_data, indicators_list):
+    """ Process the indicators list and return a dataframe with the required columns. """
+    df = graph_data.copy()  # Copy the entire dataframe initially
+    counter = {"SMA": 0, "EMA": 0, "WMA": 0, "HMA": 0, "DEMA": 0, "TEMA": 0, "TRIMA": 0, "KAMA": 0, "ZLMA": 0, "ALMA": 0, "BBANDS": 0}
+    for idx, indicator in enumerate(indicators_list):
+        
+        counter[indicator['indicator']] += 1  # Increment the counter for the current indicator type
+
+        if indicator["indicator"] == "SMA":
+            df[f"SMA_{counter['SMA']}"] = ta.sma(df["close"], length=indicator['params']['period'])
+        elif indicator["indicator"] == "EMA":
+            df[f"EMA_{counter['EMA']}"] = ta.ema(df["close"], length=indicator['params']['period'])
+        elif indicator["indicator"] == "WMA":
+            df[f"WMA_{counter['WMA']}"] = ta.wma(df["close"], length=indicator['params']['period'])
+        elif indicator["indicator"] == "HMA":
+            df[f"HMA_{counter['HMA']}"] = ta.hma(df["close"], length=indicator['params']['period'])
+        elif indicator["indicator"] == "DEMA":
+            df[f"DEMA_{counter['DEMA']}"] = ta.dema(df["close"], length=indicator['params']['period'])
+        elif indicator["indicator"] == "TEMA":
+            df[f"TEMA_{counter['TEMA']}"] = ta.tema(df["close"], length=indicator['params']['period'])
+        elif indicator["indicator"] == "TRIMA":
+            df[f"TRIMA_{counter['TRIMA']}"] = ta.trima(df["close"], length=indicator['params']['period'])
+        elif indicator["indicator"] == "KAMA":
+            df[f"KAMA_{counter['KAMA']}"] = ta.kama(df["close"], length=indicator['params']['period'])
+        elif indicator["indicator"] == "ZLMA":
+            df[f"ZLMA_{counter['ZLMA']}"] = ta.zlma(df["close"], length=indicator['params']['period'])
+        elif indicator["indicator"] == "ALMA":
+            df[f"ALMA_{counter['ALMA']}"] = ta.alma(df["close"], length=indicator['params']['period'])
+        elif indicator["indicator"] == "BBANDS":
+            bband = ta.bbands(df["close"], length=indicator['params']['length'], std=indicator['params']['std'])
+            bands = [f"BBM_{counter['BBANDS']}", f"BBU_{counter['BBANDS']}", f"BBL_{counter['BBANDS']}"]
+            for i, band in enumerate(bands):
+                df[band] = bband.iloc[:, i]
+    
+    columns_to_keep = [col for col in df.columns if col not in ['open', 'high', 'low', 'volume']]
+    return df[columns_to_keep]
 
 def get_human_readable_freq(internal_freq):
     for key, value in FREQUENCY_MAPPING.items():
@@ -155,7 +193,7 @@ def fetch_and_plot_data(graph_name_placeholder, graph_placeholder, selected_date
     if len(data) > 0:
         # Resample data to the selected frequency
         resampled_data = resample_data(data, st.session_state.selected_freq)
-        if len(resampled_data) > 1000000:
+        if len(resampled_data) > 500000:
             st.warning("The data is too big for plotting. Please select a narrower time period or a broader time frame.")
             return
         
@@ -172,28 +210,27 @@ def fetch_and_plot_data(graph_name_placeholder, graph_placeholder, selected_date
     end_time = t.time()
     st.write(f"Plot loaded in {end_time - start_time:.2f} seconds.")
 
-def plot_in_placeholder(series, placeholder):
+def plot_in_placeholder(data, placeholder):
+    # Convert to DataFrame if it's a Series
+    if isinstance(data, pd.Series):
+        data = data.to_frame(name="Value")
 
-    if not isinstance(series, pd.Series):
-        st.error("The input is not a Pandas Series.")
+    if not isinstance(data, pd.DataFrame):
+        st.error("The input is not a Pandas DataFrame.")
         return
 
-    if not isinstance(series.index, pd.DatetimeIndex):
-        st.error("The Series does not have a datetime index.")
+    if not isinstance(data.index, pd.DatetimeIndex):
+        st.error("The DataFrame does not have a datetime index.")
         return
-
-    # Convert the series to a dataframe for compatibility with plotly.express
-    df = series.reset_index()
-    df.columns = ["Date", "Value"]
 
     # Create interactive plot using plotly.express
-    fig = px.line(df, x='Date', y='Value')
-    
+    fig = px.line(data)
+
     # Check if logarithmic scale is selected
     if st.session_state.get('log_scale', False):
         fig.update_yaxes(type="log")
 
     # Update the layout to adjust the height and width
-    fig.update_layout(autosize=True,height=650)  # You can adjust the width and height values as needed
+    fig.update_layout(autosize=True, height=650)  # You can adjust the width and height values as needed
 
-    placeholder.plotly_chart(fig,use_container_width=True,theme="streamlit")
+    placeholder.plotly_chart(fig, use_container_width=True, theme="streamlit")
--- render.py
+++ render.py
@@ -4,15 +4,15 @@ import pytz
 import time as t
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
 from datetime import datetime, timedelta, time
 
 # Local Module Imports
 from constants import FREQUENCY_MAPPING
-from auxiliary import get_human_readable_freq, fetch_and_plot_data, unique_symbol_freqs, plot_in_placeholder
+from auxiliary import get_human_readable_freq, fetch_and_plot_data, unique_symbol_freqs, plot_in_placeholder, process_indicators
 
 # --------------- Page Rendering Functions ---------------
-
 
 def get_pages(session_state):
     
@@ -92,14 +92,14 @@ def get_pages(session_state):
         # Move form to main area
         with st.form(key='my_form'):
             st.title("Data to fetch:")
-            
+
             # Symbol and Frequency selectors inside the form
             symbol_selector(engine, session_state)
             frequency_selector()
 
             # Set default value for date selector to 6 months ago
             six_months_ago = datetime.today() - pd.DateOffset(months=6)
-            
+
             col1, col2 = st.columns(2)
             with col1:
                 selected_date = st.date_input("Select Start Date", six_months_ago)
@@ -123,8 +123,6 @@ def get_pages(session_state):
         if submit_button or (hasattr(st.session_state, "selected_freq") and st.session_state.selected_freq != "1h"):
             start_time = t.time()  # Start timing here
             fetch_and_plot_data(graph_name_placeholder, graph_placeholder, selected_datetime, end_datetime, start_time, engine)
-        elif hasattr(st.session_state, "graph_data") and st.session_state.graph_data is not None:
-            plot_in_placeholder(st.session_state.graph_data["close"], graph_placeholder)
 
         # Update the graph_name_placeholder with the current symbol and frequency after the form is processed
         if st.session_state.selected_symbol is not None:
@@ -135,7 +133,98 @@ def get_pages(session_state):
 
     def indicators_page(engine):
         st.title("📊 Indicators")
-        st.subheader("Coming soon!")
+
+        # Placeholder for the graph
+        graph_placeholder = st.empty()
+
+        # Dropdown for indicator selection
+        available_indicators = ["SMA", "EMA", "WMA", "HMA", "DEMA", "TEMA", "TRIMA", "KAMA", "ZLMA", "ALMA", "BBANDS"]
+
+        # Initialize the indicators list if it doesn't exist
+        if not hasattr(st.session_state, "indicators_list"):
+            st.session_state.indicators_list = []
+
+        # Create legend identifiers for the indicators
+        legend_identifiers = []
+        if st.session_state.indicators_list:
+            counter = {indicator['indicator']: 0 for indicator in st.session_state.indicators_list}
+            for item in st.session_state.indicators_list:
+                counter[item['indicator']] += 1
+                legend_identifiers.append(f"{item['indicator']} {counter[item['indicator']]}")
+
+        # Create columns for forms and indicator table
+        col1, col2 = st.columns(2)
+
+        selected_indicator = col1.selectbox("Add an Indicator", available_indicators)
+
+        # Display appropriate parameters input UI based on the indicator selected
+        with col1.form(key='indicator_form'):
+            params = {}
+            if selected_indicator in ["SMA", "EMA", "WMA", "HMA", "DEMA", "TEMA", "TRIMA", "KAMA", "ZLMA", "ALMA"]:
+                params["period"] = st.number_input(f"Enter {selected_indicator} Period", value=20, min_value=1)
+            elif selected_indicator == "BBANDS":
+                inner_col1, inner_col2 = st.columns(2)
+                with inner_col1:
+                    params["length"] = st.number_input("Length", value=20, min_value=1)
+                with inner_col2:
+                    params["std"] = st.number_input("Number of standard deviations", value=2.0, min_value=0.5, step=0.1)
+            add_indicator = st.form_submit_button(label=f"Add {selected_indicator}")
+
+        # Add the selected indicator and its parameters to the indicators list
+        if add_indicator:
+            st.session_state.indicators_list.append({"indicator": selected_indicator, "params": params})
+            # Update the legend_identifiers list after adding a new indicator
+            counter = {indicator['indicator']: 0 for indicator in st.session_state.indicators_list}
+            legend_identifiers = []
+            for item in st.session_state.indicators_list:
+                counter[item['indicator']] += 1
+                legend_identifiers.append(f"{item['indicator']} {counter[item['indicator']]}")
+
+        # Form for deleting selected indicators in the left column
+        with col1.form(key='delete_form'):
+            # Allow user to select entries to delete
+            if st.session_state.indicators_list:
+                indices_to_delete = st.multiselect("Delete an Indicator:", options=list(range(len(st.session_state.indicators_list))), format_func=lambda x: legend_identifiers[x])
+                delete_button = st.form_submit_button("Delete Selected Indicators")
+                
+                if delete_button:
+                    # Delete the selected indices from the list
+                    for index in sorted(indices_to_delete, reverse=True):
+                        del st.session_state.indicators_list[index]
+                    # Clear the selection after deletion
+                    indices_to_delete.clear()
+                    st.experimental_rerun()  # To ensure the UI updates
+
+        # Display the selected indicators in a table in the right column
+        if st.session_state.indicators_list:
+            with col2:
+                st.write("Current Indicators")
+                
+                df = pd.DataFrame(st.session_state.indicators_list)
+                df['Indicator'] = legend_identifiers
+
+                # Extract the parameters and their values
+                indicator_names = df['Indicator'].tolist()
+                list_of_dicts = df['params'].tolist()
+                
+                # Construct rows for multi-index
+                rows = []
+                for indicator, params in zip(indicator_names, list_of_dicts):
+                    for param, value in params.items():
+                        rows.append((indicator, param, value))
+                
+                # Create the multi-index DataFrame
+                multi_df = pd.DataFrame(rows, columns=["Indicator", "Parameter", "Value"]).set_index(["Indicator", "Parameter"])
+                
+                # Group by and unstack to get the desired multi-index structure
+                multi_df = multi_df.groupby(level=[0, 1]).first().unstack().stack()
+                
+                st.write(multi_df.to_html(), unsafe_allow_html=True)
+
+        # If there's data available, process and plot it
+        if hasattr(st.session_state, "graph_data") and st.session_state.graph_data is not None:
+            df = process_indicators(st.session_state.graph_data, st.session_state.indicators_list)
+            plot_dataframe(df, graph_placeholder)
 
     def bots_page(engine):
         st.title("🤖 Bots")
@@ -187,4 +276,18 @@ def datetime_selector():
     selected_minute = st.slider("Select a Minute", 0, 59, 0) # Slider from 0 to 23 for hours
     selected_datetime = datetime.combine(selected_date, time(hour=selected_hour, minute=selected_minute))
     return selected_datetime
+
+
+def plot_dataframe(df, placeholder):
+    """ Plot the dataframe's columns using plotly. """
+    fig = px.line(df)
+
+    # Check if logarithmic scale is selected
+    if st.session_state.get('log_scale', False):
+        fig.update_yaxes(type="log")
+
+    # Update the layout to adjust the height and width
+    fig.update_layout(autosize=True, height=650)  # Adjust as needed
+
+    placeholder.plotly_chart(fig, use_container_width=True, theme="streamlit")
 
